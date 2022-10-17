0f4c3d40bb5450bbf0b65c4fab147f710b305ce0
Benjamiyu <benjammy024@gmail.com>
2022-10-17 12:33:05 -0700

    Use the function puts updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* Using the function puts to print out strings*/
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts(Mystr);
-	return 0;
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
 }
