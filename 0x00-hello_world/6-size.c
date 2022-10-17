4161f3be51f98a0e4d8ad28b58e4db5feebf47a2
Benjamiyu <benjammy024@gmail.com>
2022-10-17 15:52:35 -0700

    print out types of variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,10 @@
  */
 int main(void)
 {
-char charType;
-int intType;
-long int longIntType;
-long long int longLongIntType;
-float floatType;
-
-printf("Size of a char: %lu bytes\n", (unsigned long)sizeof(charType));
-printf("Size of an int: %lu bytes\n", (unsigned long)sizeof(intType));
-printf("Size of a long int: %lu bytes\n", (unsigned long)sizeof(longIntType));
-printf("Size of a long long int: %lu bytes\n", (unsigned long)sizeof(longLongIntType));
-printf("Size of a float: %lu bytes\n", (unsigned long)sizeof(floatType));
-
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
