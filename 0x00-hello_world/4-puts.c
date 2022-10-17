a294c60ea7cbafe8206ac3dcdbf53a03ab510442
Benjamiyu <benjammy024@gmail.com>
2022-10-17 13:22:32 -0700

    using puts function to print out a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Using puts function
- *
- * Description: This is c program that uses puts function to print out a string.
+ * main - This c program uses puts function to print out a string.
  * Return: Success 0
  */
 int main(void)
